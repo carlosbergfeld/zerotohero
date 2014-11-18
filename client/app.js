eb7c9ad19686c919467a8981ef3378f13113d776
Carlos Bergfeld <carlos.bergfeld@gmail.com>
2014-11-18 01:32:44 -0200

    Autorun d3 and fixture data

--- client/app.js
+++ client/app.js
@@ -43,80 +43,11 @@ Template.task.events({
 Template.cityPage.rendered = function () {
   var self = this;
 
-  var teams = Teams.find({citySlug: 'palmas'}).fetch();
-  var fD = teams.map(function (team) {
-    var teamId = team._id;
+  Tracker.autorun(function () {
+    console.log('autoran');
 
-    var completed = Tasks.find({team: teamId, checked: true}).count();
-    var total = Tasks.find({team: teamId}).count();
-
-    return [team.name, (completed / total) * 100];
-  });
-
-  var id = "#dashboard";
-  var barColor = 'steelblue';
-
-  var hG={},    hGDim = {t: 60, r: 0, b: 30, l: 0};
-  hGDim.w = 100 * fD.length - hGDim.l - hGDim.r, 
-  hGDim.h = 300 - hGDim.t - hGDim.b;
-
-  //create svg for histogram.
-  var hGsvg = self.find('svg') || d3.select(id).append("svg")
-      .attr("width", hGDim.w + hGDim.l + hGDim.r)
-      .attr("height", hGDim.h + hGDim.t + hGDim.b).append("g")
-      .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
-
-  // create function for x-axis mapping.
-  var x = d3.scale.ordinal().rangeRoundBands([0, hGDim.w], 0.2)
-          .domain(fD.map(function(d) { return d[0]; }));
-
-  // Add x-axis to the histogram svg.
-  hGsvg.append("g").attr("class", "x axis")
-      .attr("transform", "translate(0," + hGDim.h + ")")
-      .call(d3.svg.axis().scale(x).orient("bottom"));
-
-  // Create function for y-axis map.
-  var y = d3.scale.linear().range([hGDim.h, 0])
-          .domain([0, 100]);
-
-  // Create bars for histogram to contain rectangles and freq labels.
-  var bars = hGsvg.selectAll(".bar").data(fD).enter()
-          .append("g").attr("class", "bar");
-  
-  //create the rectangles.
-  bars.append("rect")
-      .attr("x", function(d) { return x(d[0]); })
-      .attr("y", function(d) { return y(d[1]); })
-      .attr("width", x.rangeBand())
-      .attr("height", function(d) { return hGDim.h - y(d[1]); })
-      .attr('fill',barColor);// mouseout is defined below.
-      
-  //Create the frequency labels above the rectangles.
-  bars.append("text").text(function(d){ return d3.format(",")(d[1])})
-      .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
-      .attr("y", function(d) { return y(d[1])-5; })
-      .attr("text-anchor", "middle");
-  
-  // create function to update the bars. This will be used by pie-chart.
-  var updateHistogram = function (nD, color) {
-      // Attach the new data to the bars.
-      var bars = hGsvg.selectAll(".bar").data(nD);
-      
-      // transition the height and color of rectangles.
-      bars.select("rect").transition().duration(500)
-          .attr("y", function(d) {return y(d[1]); })
-          .attr("height", function(d) { return hGDim.h - y(d[1]); })
-          .attr("fill", color);
-
-      // transition the frequency labels location and change value.
-      bars.select("text").transition().duration(500)
-          .text(function(d){ return d3.format(",")(d[1])})
-          .attr("y", function(d) {return y(d[1])-5; });            
-  }
-
-  Deps.autorun(function () {
-    teams = Teams.find({citySlug: 'palmas'}).fetch();
-    freqData = teams.map(function (team) {
+    var teams = Teams.find({citySlug: 'palmas'}).fetch();
+    var fD = teams.map(function (team) {
       var teamId = team._id;
 
       var completed = Tasks.find({team: teamId, checked: true}).count();
@@ -125,6 +56,70 @@ Template.cityPage.rendered = function () {
       return [team.name, (completed / total) * 100];
     });
 
-    updateHistogram(freqData, 'steelBlue');
+    var id = "#dashboard";
+    var barColor = 'steelblue';
+
+    var hG={},    hGDim = {t: 60, r: 0, b: 30, l: 0};
+    hGDim.w = 100 * fD.length - hGDim.l - hGDim.r, 
+    hGDim.h = 300 - hGDim.t - hGDim.b;
+
+    //create svg for histogram.
+    var hGsvg = d3.select('svg').select('g');
+
+    if (hGsvg[0][0] === null) {
+        hGsvg = d3.select(id).append("svg")
+                .attr("width", hGDim.w + hGDim.l + hGDim.r)
+                .attr("height", hGDim.h + hGDim.t + hGDim.b).append("g")
+                .attr("transform", "translate(" + hGDim.l + "," + hGDim.t + ")");
+    }
+
+    // create function for x-axis mapping.
+    var x = d3.scale.ordinal().rangeRoundBands([0, hGDim.w], 0.2)
+            .domain(fD.map(function(d) { return d[0]; }));
+
+    d3.select('.axis').remove();
+    console.log(hGDim.h);
+    // Add x-axis to the histogram svg.
+    hGsvg.append("g").attr("class", "x axis")
+        .attr("transform", "translate(0," + hGDim.h + ")")
+        .call(d3.svg.axis().scale(x).orient("bottom"));
+
+    // Create function for y-axis map.
+    var y = d3.scale.linear().range([hGDim.h, 0])
+            .domain([0, 100]);
+
+    // Create bars for histogram to contain rectangles and freq labels.
+    var bars = hGsvg.selectAll(".bar")
+        .data(fD, function (team) {
+          return team[0];
+        });
+
+    var barsEnter = bars.enter().append("g").attr("class", "bar")
+    
+    //create the rectangles.
+    barsEnter.append("rect")
+        .attr("x", function(d) { return x(d[0]); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("width", x.rangeBand())
+        .attr("height", function(d) { return hGDim.h - y(d[1]); })
+        .attr('fill',barColor);// mouseout is defined below.
+        
+    //Create the frequency labels above the rectangles.
+    barsEnter.append("text").text(function(d){ return d3.format(",")(d[1])})
+        .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
+        .attr("y", function(d) { return y(d[1])-5; })
+        .attr("text-anchor", "middle");
+    
+    // transition the height and color of rectangles.
+    bars.select("rect").transition().duration(500)
+        .attr("y", function(d) {return y(d[1]); })
+        .attr("height", function(d) { return hGDim.h - y(d[1]); })
+        .attr("fill", barColor);
+
+    // transition the frequency labels location and change value.
+    bars.select("text").transition().duration(500)
+        .text(function(d){ return d3.format(",")(d[1])})
+        .attr("y", function(d) {return y(d[1])-5; });            
+
   });
 };
--- server/app.js
+++ server/app.js
@@ -31,4 +31,91 @@ Meteor.startup(function () {
     Accounts.sendEnrollmentEmail(userId);
   }
 
+
+  // fixtures
+  if (Teams.find().count() === 0) {
+    Teams.insert({
+      name: 'Vigilantes',
+      nameSlug: 'vigilantes',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'Vigie tudo ao seu redor'
+    });
+    Teams.insert({
+      name: 'Twitter',
+      nameSlug: 'twitter',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'Conte e receba notícias do mundo inteiro'
+    });
+    Teams.insert({
+      name: 'Stripe',
+      nameSlug: 'stripe',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'Pagaments para startups'
+    });
+    Teams.insert({
+      name: 'Postmates',
+      nameSlug: 'postmates',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'Receba seus pratos favoritos de qualquer restaurante em sua casa'
+    });
+    Teams.insert({
+      name: 'Slack',
+      nameSlug: 'slack',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'Comunicação de time, sem email'
+    });
+    Teams.insert({
+      name: 'Uber',
+      nameSlug: 'uber',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'Um carro à sua disposição, com um toque no seu celular'
+    });
+    Teams.insert({
+      name: 'Facebook',
+      nameSlug: 'facebook',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'Fofoque com todos seus amigos'
+    });
+    Teams.insert({
+      name: 'Airbnb',
+      nameSlug: 'airbnb',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'Hospéde-se por um preço muito menor em lares em 190 países'
+    });
+    Teams.insert({
+      name: 'Buffer',
+      nameSlug: 'buffer',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'A forma mais fácil de publicar nas mídias sociais'
+    });
+    Teams.insert({
+      name: 'ZeroToHero',
+      nameSlug: 'zerotohero',
+      city: 'Palmas',
+      citySlug: 'palmas',
+      pitch: 'Acompanhe o desempenho dos times do StartupWeekend'
+    });
+
+    var teams = Teams.find().fetch();
+    _.each(teams, function (team) {
+      _.each(CHECKLIST, function (task, index) {
+        Tasks.insert({
+          position: index,
+          team: team._id,
+          text: task.text,
+          hasCounter: task.hasCounter
+        });
+      });
+    });
+  }
+
 });
